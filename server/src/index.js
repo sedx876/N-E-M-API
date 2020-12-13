16de1d40bacc7f806552f581db00069d1e1cc46c
sedx876 <swatkins876@gmail.com>
2020-12-13 06:19:38 -0500

    cors.js

--- server/src/index.js
+++ server/src/index.js
@@ -11,10 +11,14 @@ const cors = require('./cors')
 const routes = require('./routes')
 
 mongoose.Promise = global.Promise
-mongoose.connect('mongodb://localhost/strain-domain', { useNewUrlParser: true })
+mongoose.connect('mongodb://localhost/strain-domain', { useNewUrlParser: true }, {useUnifiedTopology: true})
 mongoose.set('useFindAndModify', false)
 
-app.use(bodyParse.urlencoded({ extended: true }))
+app.use(bodyParser.urlencoded({ extended: true }))
 app.use(bodyParser.json())
 
 //cors(app)
+//routes(app)
+
+app.listen(port)
+console.log(`Strain Domain is running on ${port}`)
